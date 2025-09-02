7221a1622d3faaff8e72ba6c9d29f4e0cf358b60
Diosvely Perez Arteaga <136009021+Diosvely@users.noreply.github.com>
2025-09-02 22:27:52 +0100

    Crear carpetas por ERP

---
+++ ERP Microsoft/ERP Microsoft Dynamics 365 (Business Central)/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "ddfa4daa-a232-49d3-9de0-5ab33ad9f550",
+# META       "default_lakehouse_name": "lh_crudo_ERPmicrosoft",
+# META       "default_lakehouse_workspace_id": "d2933686-1449-435d-bcfc-e34b63913adb",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "ddfa4daa-a232-49d3-9de0-5ab33ad9f550"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
